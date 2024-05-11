4515d99c471b751aca29a3dbbfed57899f44532f
rar34 <ra.cse34@gmail.com>
2024-05-12 00:34:12 +0600

    added download apps section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import GetApp from "../../GetApp/GetApp";
 import JobsFeatures from "../../JobsFeature/JobsFeatures";
 import JobCategory from "../JobCategory/JobCategory";
 import Banner from "./Banner/Banner";
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <JobCategory></JobCategory>
             <JobsFeatures></JobsFeatures>
+            <GetApp></GetApp>
         </div>
     );
 };
