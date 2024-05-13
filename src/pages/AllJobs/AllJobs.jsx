574693d580d29337753610aca310875964832fce
rar34 <ra.cse34@gmail.com>
2024-05-13 23:51:54 +0600

    added search function in all jobs section

--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -9,6 +9,7 @@ const AllJobs = () => {
     // const jobs = useLoaderData();
     // const [jobs, setJobs] = useState([]);
     // console.log(jobs)
+    const [searchQuery, setSearchQuery] = useState('');
 
     const { isPending, error, isError, data: jobs } = useQuery({
         queryKey: ['jobs'],
@@ -28,16 +29,29 @@ const AllJobs = () => {
         return <p>{error.message}</p>
     }
 
-    // if(jobs.length === 0){
-    //     return <p>No jobs available....</p>
-    // }
+
+    // console.log(jobs)
+
+    const handleSearchChange = (e) => {
+        const query = e.target.value;
+        setSearchQuery(query);
+    };
+
+    const filteredJobs = jobs.filter(job =>
+        job?.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+
+    if (jobs.length === 0) {
+        return <p>No jobs available....</p>
+    }
 
 
     return (
         <div>
             <div className="flex justify-center my-10">
                 <label className="input rounded-none rounded-l-lg input-bordered flex items-center gap-2">
-                    <input name="searchText" type="text" className="grow" placeholder="Search your job" />
+                    <input value={searchQuery} onChange={handleSearchChange} name="searchText" type="text" className="grow" placeholder="Search your job" />
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
                 </label>
                 <button className="p-3 bg-green-700 text-white font-bold rounded-r-lg">Search</button>
@@ -55,7 +69,7 @@ const AllJobs = () => {
                     </thead>
                     <tbody>
                         {
-                            jobs.map((job) => <tr key={job._id}>
+                            filteredJobs.map((job) => <tr key={job._id}>
                                 <td>{job.title}</td>
                                 <td>{new Date(job.posting_date).toLocaleDateString()}</td>
                                 <td>{new Date(job.deadline).toLocaleDateString()}</td>
--- src/pages/AppliedJobs/AppliedJobs.jsx
+++ src/pages/AppliedJobs/AppliedJobs.jsx
@@ -1,28 +1,69 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../components/AuthProvider/AuthProvider";
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
 const AppliedJobs = () => {
-
+    const [jobs, setJobs] = useState([]);
     const { user } = useContext(AuthContext);
+    const [sortBy, setSortBy] = useState(jobs)
 
-    const { isPending, error, isError, data: jobs } = useQuery({
-        queryKey: ['jobs'],
-        queryFn: async () => {
-            const res = await fetch(`${import.meta.env.VITE_API_URL}/appliedJobs/${user?.email}`, { credentials: 'include' });
-            return res.json();
-        }
-    })
-    // console.log(jobs)
 
-    if (isPending) {
-        return <div className='flex justify-center items-center text-3xl'><span className="loading loading-spinner loading-lg"></span></div>
-    }
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/appliedJobs/${user?.email}`, { credentials: 'include' })
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [user])
 
-    if (isError) {
-        return <p>{error.message}</p>
-    }
+
+    const handleSortChange = (e) => {
+        const searchText = e.target.value;
+        setSortBy(searchText);
+        // let sortedJobs = [...jobs];
+
+        
+        // if(searchText === ""){
+        //     setSortBy(jobs)
+        // }
+
+        // if(searchText === 'Onsite Job'){
+        //     const sortedJobs = jobs.filter(job => job.category === 'Onsite Job')
+        //     setSortBy(sortedJobs)
+        // }
+        // if(searchText === 'Remote Job'){
+        //     const sortedJobs = jobs.filter(job => job.category === 'Remote Job')
+        //     setSortBy(sortedJobs)
+        // }
+        // if(searchText === 'Hybrid Job'){
+        //     const sortedJobs = jobs.filter(job => job.category === 'Hybrid Job')
+        //     setSortBy(sortedJobs)
+        // }
+        // if(searchText === 'Part Time Job'){
+        //     const sortedJobs = jobs.filter(job => job.category === 'Part Time Job')
+        //     setSortBy(sortedJobs)
+        // }
+        
+    };
+
+    const filteredJobs = sortBy ? jobs.filter(job => job.category === sortBy) : jobs;
+
+
+    // const { isPending, error, isError, data: jobs } = useQuery({
+    //     queryKey: ['jobs'],
+    //     queryFn: async () => {
+    //         const res = await fetch(`${import.meta.env.VITE_API_URL}/appliedJobs/${user?.email}`, { credentials: 'include' });
+    //         return res.json();
+    //     }
+    // })
+    // // console.log(jobs)
+
+    // if (isPending) {
+    //     return <div className='flex justify-center items-center text-3xl'><span className="loading loading-spinner loading-lg"></span></div>
+    // }
+
+    // if (isError) {
+    //     return <p>{error.message}</p>
+    // }
 
 
     if (jobs.length === 0) {
@@ -34,8 +75,8 @@ const AppliedJobs = () => {
             {/* <h2>Applied jobs {jobs.length}</h2> */}
             <div className="text-center mb-6">
                 <span className="text-xl font-bold">Filter By: </span>
-                <select className="border text-lg font-medium bg-[#00385E] text-white p-3 rounded-lg" name="jobCategory" id="jobCategory" required>
-                    <option value="">Select Category</option>
+                <select value={sortBy} onChange={handleSortChange} className="border text-lg font-medium bg-[#00385E] text-white p-3 rounded-lg" name="jobCategory" id="jobCategory" required>
+                    <option value="">All Jobs</option>
                     <option value="Onsite Job">Onsite Job</option>
                     <option value="Remote Job">Remote Job</option>
                     <option value="Hybrid Job">Hybrid Job</option>
@@ -50,16 +91,18 @@ const AppliedJobs = () => {
                             <th>Job Posting Date</th>
                             <th>Deadline</th>
                             <th>Salary Range</th>
+                            <th>Job Category</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            jobs.map((job) => <tr key={job._id}>
+                            filteredJobs.map((job) => <tr key={job._id}>
                                 <td>{job.title}</td>
                                 <td>{new Date(job.posting_date).toLocaleDateString()}</td>
                                 <td>{new Date(job.deadline).toLocaleDateString()}</td>
                                 <td>{job.min_salary}-{job.max_salary}</td>
+                                <td>{job.category}</td>
                                 <Link to={`/job/${job._id}`}><button className="btn bg-[#00385E] btn-sm text-white" tabIndex="0" role="link">View Details</button></Link>
                             </tr>)
                         }
